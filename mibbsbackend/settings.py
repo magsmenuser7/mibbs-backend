27ae42c79bc4e7bc03ea2cf8ffeca3f74b360265
magsmenuser7 <magsmenuser7@gmail.com>
2025-07-05 19:44:36 +0530

    static folder added

--- mibbsbackend/settings.py
+++ mibbsbackend/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
+# TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
 STATIC_DIRS = os.path.join(BASE_DIR,'static')
 
 
